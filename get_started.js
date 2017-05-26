a84e9d735ec53b867e6a28c72b3d68a833b37553
Jevan Wu <jevanwu@gmail.com>
2017-05-26 17:38:30 +0800

    add new event

--- get_started.js
+++ get_started.js
@@ -1,69 +1,95 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
-    }
-  })
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
 
-  var app2 = new Vue({
-    el: '#app-2',
-    data: {
-      message: 'You loaded this page on ' + new Date()
-    }
-  })
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date()
+  }
+})
 
-  var app3 = new Vue({
-    el: '#app-3',
-    data: {
-      seen: true
-    }
-  })
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
 
-  var app4 = new Vue({
-    el: '#app-4',
-    data: {
-      todos: [
-        { text: 'Learn JavaScript' },
-        { text: 'Learn Vue' },
-        { text: 'Build something awesome' }
-      ]
-    }
-  })
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
 
-  var app5 = new Vue({
-    el: '#app-5',
-    data: {
-      message: 'Hello Vue.js!'
-    },
-    methods: {
-      reverseMessage: function () {
-        this.message = this.message.split('').reverse().join('')
-      }
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
     }
-  })
+  }
+})
 
-  var app6 = new Vue({
-    el: '#app-6',
-    data: {
-      message: 'Hello Vue!'
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+// Define a new component called todo-item
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { text: 'Vegetables' },
+      { text: 'Cheese' },
+      { text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
+
+// 父子组件的关系可以总结为 props down, events up
+Vue.component('button-counter', {
+  template: '<button v-on:click="increment">{{ counter }}</button>',
+  data: function () {
+    return {
+      counter: 0
     }
-  })
-
-  // Define a new component called todo-item
-  Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
-  })
-
-  var app7 = new Vue({
-    el: '#app-7',
-    data: {
-      groceryList: [
-        { text: 'Vegetables' },
-        { text: 'Cheese' },
-        { text: 'Whatever else humans are supposed to eat' }
-      ]
+  },
+  methods: {
+    increment: function () {
+      this.counter += 1
+      this.$emit('increment')
     }
-  })
-});
+  },
+})
+
+var app8 = new Vue({
+  el: '#app-8',
+  data: {
+    total: 0
+  },
+  methods: {
+    incrementTotal: function () {
+      this.total += 1
+    }
+  }
+})
